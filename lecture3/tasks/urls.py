acf9a68ffb08a20663cb58f78ea852068dd4ec85
Vladimir Lopez <vladimir.luna96@hotmail.com>
2023-09-27 01:56:27 -0600

    Add app_name for inheritance, django-forms, class

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks" #Esto se agrega para evita name collisions con index
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
